35d28a2433ddc41a4f28427d78fee6ed8f915d96
han900204 <54014070+han900204@users.noreply.github.com>
2022-02-21 12:37:58 -0800

    updates

--- client/containers/LoginContainer.tsx
+++ client/containers/LoginContainer.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
+import { Box } from '@material-ui/core';
 
 const LoginContainer = () => {
-  return (
-    <>
-      <div>
-        <LoginForm />
-      </div>
-      <div>
-        <Link to='/signup'>Sign Up</Link>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Box
+				display='flex'
+				alignItems='center'
+				justifyContent='center'
+				sx={{ m: '5rem' }}
+			>
+				<LoginForm />
+			</Box>
+			<Box
+				display='flex'
+				alignItems='center'
+				justifyContent='center'
+				sx={{ m: '10rem' }}
+			>
+				Do not have account?&nbsp;&nbsp;
+				<Link to='/signup'>Sign Up</Link>
+			</Box>
+		</>
+	);
 };
 
 export default LoginContainer;
