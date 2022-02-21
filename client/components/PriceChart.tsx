afe26deebb2d467723b17cabadf4cde36ecc61ba
han900204 <54014070+han900204@users.noreply.github.com>
2022-02-20 18:14:26 -0800

    updates

--- client/components/PriceChart.tsx
+++ client/components/PriceChart.tsx
@@ -1,29 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import TimeseriesChart from './chartComponents/TimeseriesChart/index';
+import { Box } from '@material-ui/core';
 
 const PriceChart = ({ priceData }) => {
+	const [width, setWidth] = useState<undefined | number>();
+	const ref = useRef<null | HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (ref?.current?.parentElement?.offsetWidth) {
+			setWidth(ref.current.parentElement.offsetWidth * 0.8);
+		}
+		window.addEventListener('resize', () => {
+			if (ref?.current?.parentElement?.offsetWidth) {
+				setWidth(ref.current.parentElement.offsetWidth * 0.8);
+			}
+		});
+	});
+
 	priceData = priceData.map((data) => ({
 		label: new Date(data['date']),
 		value: data['price'],
 		tooltipContent: `$${data['price'].toFixed(2)}`,
 	}));
 
-	console.log('priceData', priceData);
-
 	return (
-		<TimeseriesChart
-			svgProps={{
-				width: 800,
-				height: 800,
-				margin: { top: 50, right: 50, bottom: 50, left: 50 },
-			}}
-			axisProps={{
-				xLabel: 'Time',
-				yLabel: 'Price',
-			}}
-			data={priceData}
-			strokeWidth={1}
-		/>
+		<>
+			<Box display='flex' alignItems='center' justifyContent='center'>
+				<div ref={ref} />
+				<TimeseriesChart
+					svgProps={{
+						width: width,
+						height: 400,
+						margin: { top: 50, right: 50, bottom: 50, left: 50 },
+					}}
+					axisProps={{
+						xLabel: 'Time',
+						yLabel: 'Price',
+					}}
+					data={priceData}
+					strokeWidth={1}
+				/>
+			</Box>
+		</>
 	);
 };
 
